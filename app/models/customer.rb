3220c761f3c104dfe0e7b27c7607a4acf9196a37
kobira-kk <foxmancoffee@yahoo.co.jp>
2018-05-03 11:39:34 +0900

    顧客詳細作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  def full_name
+  family_name + given_name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-resorces :customers
-root 'customers#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :customers
+  root 'customers#index'
 end
